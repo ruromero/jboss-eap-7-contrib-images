fd32abdd9793d85bb29e5e41c1269d31ff1d0f43
Ruben Romero Montes <rromerom@redhat.com>
2018-06-20 13:24:19 +0200

    Improved build script

--- build.sh
+++ build.sh
@@ -6,7 +6,46 @@ if [ -n "${DEBUG:-}" ] ; then
     set -x
 fi
 
-DRIVERS=( db2-driver-image derby-driver-image mssql-driver-image oracle-driver-image mariadb-driver-image )
+function print_help() {
+    echo "Builds and pushes JDBC Driver images to a docker registry."
+    echo ""
+    echo "Usage: "
+    echo "   build.sh <DRIVER_1> [<DRIVER_2>...] [--registry=myregistry.example.com:5000]"
+    echo "Options:"
+    echo "   Available driver images to build: db2,derby,mssql,oracle,mariadb"
+    echo "   --registry   Specifies the docker registry to use for tagging and pushing. Defaults to docker-registry.default.svc:5000"
+}
+
+declare -a drivers=()
+
+while (($#))
+do
+    case $1 in
+        db2|derby|mssql|oracle|mariadb)
+            drivers+=($1-driver-image)
+        ;;
+        --registry=*)
+            registry=${1#*=}
+        ;;
+        -h)
+            print_help
+            exit 0
+        ;;
+        --help)
+            print_help
+            exit 0
+        ;;
+    esac
+shift
+done
+
+if [[ ${#drivers[@]} -eq 0 ]]
+then
+    print_help
+    exit 1
+fi
+
+registry=${registry:-docker-registry.default.svc:5000}
 
 function build() {
     local driver=$1
@@ -28,15 +67,15 @@ function docker_login() {
         echo "You must be logged in"
         exit 1
     fi
-    docker login -u `oc whoami` -p `oc whoami -t` docker-registry.default.svc:5000
+    docker login -u `oc whoami` -p `oc whoami -t` $registry
 }
 
 docker_login
 
-for driver in "${DRIVERS[@]}"
+for driver in "${drivers[@]}"
 do
     version=$(grep version ${driver}/Dockerfile | awk -F"=" '{print $2}' | sed 's/"//g')
-    tag=docker-registry.default.svc:5000/openshift/$driver:$version
+    tag=$registry/openshift/$driver:$version
     build $driver $tag
     push $tag
 done
